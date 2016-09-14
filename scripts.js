455e93a42d2e85cf8a0150dead9324774449ae71
waterseeker <wayneburris@Waynes-MacBook-Air.local>
2016-09-14 11:45:09 -0500

    still need sibling code

--- scripts.js
+++ scripts.js
@@ -1,18 +1,73 @@
-var btn = document.createElement ("button");
+var btn = document.createElement("button");
+var text = document.createTextNode("click here");
 
-var text = document.createTextNode ("CLICK MEH!");
+//create global counter to count squares on the page
+var counter = 0;
 
+btn.addEventListener('click', addSquare);
+btn.style.margin = '20px';
+btn.style.display = 'inline';
 btn.appendChild(text);
-
-btn.addEventListener ("click", addSquare);
-
 document.body.appendChild(btn);
 
+//adding square
 function addSquare() {
-    
-}
+    //creating square
+    var square = document.createElement('div');
 
-var square = document.createdElement('div');
-    square.style.backgroundColor = 'red';
-    square.style.height = '7em';
-    square.style.width = '7em';
+    //modifying square
+    square.style.backgroundColor='black';
+    square.style.height='50px';
+    square.style.width='50px';
+    square.style.display="inline-block";
+    square.style.margin='10px';
+    square.style.textAlign='center';
+    square.style.lineHeight='50px';
+
+    //increase counter everytime you addSquare
+    counter++;
+    square.innerHTML = counter;
+
+    //assign counter as the id
+    square.id = counter;
+
+    //event listeners for every square
+    square.addEventListener('click', getRandomColor);
+    square.addEventListener('mouseover', displayNumber);
+    square.addEventListener('mouseout', removeNumber);
+    square.addEventListener('dblclick', removeSquare);
+
+    //put the square on the page
+    document.body.appendChild(square);
+
+
+    function getRandomColor() {
+        //generate random color
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        //make random color background
+        square.style.backgroundColor = color;
+    }
+
+    function displayNumber() {
+        square.style.color = 'white';
+    }
+
+    function removeNumber() {
+        square.style.color= 'transparent';
+    }
+
+    function removeSquare() {
+        if (counter % 2 == 0) {
+            square.nextElementSibling.remove();
+    }
+    else {
+            square.previousElementSibling.remove();
+    }
+
+    }
+    };
